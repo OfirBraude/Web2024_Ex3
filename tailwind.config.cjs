b8845c5f40e1175cd1ac050d3cf9fd03f1eb5bb1
OFIR UZIEL <OFIRUZI@rafael.co.il>
2024-07-01 17:52:44 +0300

    test

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-  content: ["./*.{html,js,ts}"],
-  darkMode: 'class',
+  content: ["./**/*.{html,js,ts}"], // Include all subdirectories
+  darkMode: 'class', // Enable dark mode with class strategy
   theme: {
     extend: {},
   },
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio")
+    require("@tailwindcss/aspect-ratio"),
   ],
 };
